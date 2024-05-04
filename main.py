dcaba835fd6874ad82fcb4d1a34abb684a966f4a
Levi <leviszaboo@icloud.com>
2024-05-04 22:42:28 +0200

    add command line args to main script

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import logging
 import time
+import argparse
 
 from src.dataset.stations import create_stations_data
 from src.dataset.main import create_main_dataset
+from src.analysis.main import run_analysis
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
@@ -11,20 +13,60 @@ def main():
     """
     Main entry point of the application.
     """
-    logging.info('Starting data processing...')
-    start_time = time.time()
+    parser = argparse.ArgumentParser(description="Run specific parts of the application or the full pipeline.")
+    parser.add_argument(
+        '--dataset_only',
+        action='store_true',
+        help='Flag to run data creation tasks only.'
+    )
+    parser.add_argument(
+        '--analysis_only',
+        action='store_true',
+        help='Flag to run analysis tasks only.'
+    )
+    parser.add_argument(
+        '--skip_station_data',
+        action='store_true',
+        help='Flag to skip the creation of the stations dataset.'
+    )
 
-    try:
-        # Create the stations dataset
-        create_stations_data()
+    args = parser.parse_args()
 
-        # Create the main dataset
-        create_main_dataset()
+    # Default behavior: execute both tasks if no arguments are specified
+    run_data_creation = args.dataset_only or not (args.dataset_only or args.analysis_only)
+    analysis_only_tasks = args.analysis_only or not (args.dataset_only or args.analysis_only)
 
-        logging.info(f'Data processing completed in {time.time() - start_time:.2f} seconds.')
-    except Exception as e:
-        logging.error(f'An error occurred during data processing: {e}')
-        return
+
+    if run_data_creation:
+        logging.info('Starting data processing...')
+        start_time = time.time()
+
+        try:
+            # Create the stations dataset
+            if not args.skip_station_data:
+                create_stations_data()
+
+            # Create the main dataset
+            create_main_dataset()
+
+            logging.info(f'Data processing completed in {time.time() - start_time:.2f} seconds.')
+        except Exception as e:
+            logging.error(f'An error occurred during data processing: {e}')
+            return
+
+    if analysis_only_tasks:
+        logging.info('Starting analysis...')
+        start_time = time.time()
+
+        # Wait for dataset to save
+        time.sleep(1)
+
+        try:
+            run_analysis()
+            logging.info(f'Analysis completed in {time.time() - start_time:.2f} seconds.')
+        except Exception as e:
+            logging.error(f'An error occurred during analysis: {e}')
+            return
 
 if __name__ == '__main__':
     main()
